c67a7789309425ae87d14c91ee97da3949d359eb
Kangmin Lee <kang7km@gmail.com>
2023-08-20 15:07:02 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const a = 5;
+const b = 2;
+let myName = "nico";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+
+myName = "nicolas";
+
+console.log("your new name is " + myName);
